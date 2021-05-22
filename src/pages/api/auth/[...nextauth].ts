1fbe8b914d33066e49792f2768236ba0bcd9199f
Thiago Ribeiro <thiago.santos@selfpay.com.br>
2021-05-22 19:40:10 -0300

    dev

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { fauna } from '../../../services/faunadb'
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,41 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+
+      const { email } = user;
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email),
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email),
+              )
+            )
+          )
+        );
+  
+        return true;
+
+      } catch {
+        console.log('error');
+        return false;
+      }
+    },
+  }
 });
